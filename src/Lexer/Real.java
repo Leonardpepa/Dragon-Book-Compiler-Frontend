daaceef2233b6d722c8449c48af65142936dcb06
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-07-29 19:30:04 +0300

    created Real class to hold the value of real numbers

---
+++ src/Lexer/Real.java
@@ -0,0 +1,16 @@
+package Lexer;
+
+public class Real extends Token{
+
+    public final float value;
+
+    public Real(float value){
+        super(Tag.REAL);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
