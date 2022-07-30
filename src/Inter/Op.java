46a65b5ce352cd290e7bb7b259b382abd030e587
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-07-30 16:39:22 +0300

    created class Temp

---
+++ src/Inter/Op.java
@@ -0,0 +1,16 @@
+package Inter;
+
+import Lexer.Token;
+
+public class Op extends Expr{
+    public Op(Token op, Token type) {
+        super(op, type);
+    }
+
+//    @Override
+//    public Expr reduce(){
+//        Expr x = gen();
+//
+//    }
+
+}
