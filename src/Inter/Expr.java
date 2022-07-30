eda10965dd27d866b4a4e1d5b998e0f6483292db
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-07-30 21:35:52 +0300

    added code for jumps in expr

--- src/Inter/Expr.java
+++ src/Inter/Expr.java
@@ -20,8 +20,20 @@ public class Expr extends Node{
         return this;
     }
 
-    // TODO write code for conditions and jumps
+    public void jumping(int t, int f){
+        emitJumps(this.toString(), t, f);
+    }
 
+    public void emitJumps(String test, int t, int f){
+        if ( t != 0 && f != 0){
+            emit("if " + test + " goto L" + t);
+            emit("goto L" + f);
+        }else if ( t != 0){
+            emit("if " + test + " goto L" + t);
+        }else if ( f != 0){
+            emit("iffalse " + test + " goto L" + f);
+        }
+    }
 
 
     @Override
