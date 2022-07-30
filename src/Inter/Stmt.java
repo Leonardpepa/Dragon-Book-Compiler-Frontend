bae8f811c0f618640a06cb8b235f694eda88031e
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-07-30 22:49:17 +0300

    base class for statments

---
+++ src/Inter/Stmt.java
@@ -0,0 +1,13 @@
+package Inter;
+
+public class Stmt extends Node{
+
+    public static Stmt Null = new Stmt();
+    public static Stmt Enclosing = Null;
+
+    int after = 0;
+    public Stmt(){
+
+    }
+    public void gen(int a, int b){}
+}
