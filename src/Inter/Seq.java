0bbf0522bd4caec136b26b3cd38e9289b3db0118
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-07-31 17:32:59 +0300

    Seq

--- src/Inter/Seq.java
+++ src/Inter/Seq.java
@@ -11,6 +11,16 @@ public class Seq extends Stmt{
 
     @Override
     public void gen(int b, int a) {
-        super.gen(a, b);
+        if (stmt1 == Stmt.Null){
+            stmt2.gen(b, a);
+        }else if (stmt2 == Stmt.Null){
+             stmt1.gen(b, a);
+        }else{
+            int label = newLabel();
+            stmt1.gen(b, label);
+            emitLabel(label);
+            stmt2.gen(label, a);
+        }
+
     }
 }
