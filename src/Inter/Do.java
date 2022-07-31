66c04c353ce7870027419fb01c37928a62bc654d
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-07-31 17:18:03 +0300

    Class Do

---
+++ src/Inter/Do.java
@@ -0,0 +1,33 @@
+package Inter;
+
+import Symbols.Type;
+
+public class Do extends Stmt{
+
+    public Expr expr;
+    public Stmt stmt;
+
+    public Do(){
+        expr = null;
+        stmt = null;
+    }
+
+    public void init(Expr expr, Stmt stmt){
+        this.expr = expr;
+        this.stmt = stmt;
+
+        if(expr.type != Type.Bool){
+            expr.error("Boolean is required in while^");
+        }
+    }
+
+
+    @Override
+    public void gen(int a, int b) {
+        after = a;
+        int label = newLabel();
+        stmt.gen(b, label);
+        emitLabel(label);
+        expr.jumping(b, 0);
+    }
+}
