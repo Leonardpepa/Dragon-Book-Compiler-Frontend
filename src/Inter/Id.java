2aa719329b01fe6211315c5ef230916ec40186df
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-07-31 19:58:46 +0300

    almost finished projext

--- src/Inter/Id.java
+++ src/Inter/Id.java
@@ -1,11 +1,12 @@
 package Inter;
 
 import Lexer.Token;
+import Symbols.Type;
 
 public class Id extends Expr{
     public int offset;
 
-    public Id(Token op, Token type, int offset) {
+    public Id(Token op, Type type, int offset) {
         super(op, type);
         this.offset = offset;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
+import Lexer.Lexer;
+import Parser.Parser;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args){
-//        Lexer lex = new Lexer();
-//
-//        Parser parser = new Parser(lex);
-//
-//        parser.program();
-          System.out.write('\n');
+        Lexer lex = new Lexer();
+
+        Parser parser = null;
+        try {
+            parser = new Parser(lex);
+            parser.program();
+            System.out.write('\n');
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
